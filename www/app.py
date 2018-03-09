1ce976bf53a73d11261754a897c9448cea55e02a
ZingP <605156398@qq.com>
2018-03-09 13:35:49 +0800

    防止浏览器拦截

--- www/app.py
+++ www/app.py
@@ -11,15 +11,19 @@ from aiohttp import web
 
 def index(request):
     print("hello")
-    return web.Response(body='您好 pyblog ')
+    print(request)
+    body = '<h1> Hello pyblog </h1>'
+    resp = web.Response(body=body.encode("utf-8"))
+    # 如果不添加content_type，某些严谨的浏览器会把网页当成文件下载，而不是直接显示
+    resp.content_type = 'text/html;charset=utf-8'
+    return resp
 
 @asyncio.coroutine
 def init(loop):
     app = web.Application(loop=loop)
     app.router.add_route('GET', '/', index)
-    srv = yield from loop.create_server(app.make_handler(),
-                                        '127.0.0.1', 9000)
-    logging.info("server sarted at http://127.0.0.1:9000")
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info("server started at http://127.0.0.1:9000")
     return srv
 
 
